271e67aca4c1f1cf21826ace3d7906b9c0c0a375
Nader <na511dr@hotmail.com>
2019-05-08 01:13:19 +0300

    Everything is working here, Next step is playing against the computer

--- root/js/process.js
+++ root/js/process.js
@@ -1,6 +1,10 @@
 let letter = null;
 let input = [];
 let winner = false;
+let p1 = 0;
+let p2 = 0;
+let tie = 0;
+let tieCounter = 0;
 
 const chooseSide = function () {
     $('#x').on('click', function () {
@@ -21,55 +25,72 @@ const chooseSide = function () {
 //     })
 // };
 // $('.game-board').on('mouseenter' , hvr);
+const showWinner = function (c1,c2,c3 , winner) {
+    $(c1).addClass('winner');
+    $(c2).addClass('winner');
+    $(c3).addClass('winner');
+    if (!winner) {
+        p1++
+    }
+    else if (winner) {
+        p2++
+    }
 
+    $('#o').text(p2);
+    $('#x').text(p1);
+
+};
 const checkWinner = function () {
     if (input[0] !== undefined) {
         if (input[0] === input[1] && input[1] === input[2]) {
-            console.log('The winner is "' + input[0].toUpperCase() + '"');
+            showWinner('#cell-1 > h3' , '#cell-2 > h3' , '#cell-3 > h3' , letter);
             winner = true;
         }
 
         if (input[0] === input[3] && input[3] === input[6]) {
-            console.log('The winner is "' + input[0].toUpperCase() + '"');
+            showWinner('#cell-1 > h3' , '#cell-4 > h3' , '#cell-7 > h3' , letter);
             winner = true;
         }
     }
 
-    if (input[2] !== undefined) {
+     if (input[2] !== undefined) {
 
         if (input[2] === input[5] && input[5] === input[8]) {
-            console.log('The winner is "' + input[2].toUpperCase() + '"');
+            showWinner('#cell-3 > h3' , '#cell-6 > h3' , '#cell-9 > h3' , letter);
             winner = true;
 
         }
 
     }
-    if (input[4] !== undefined) {
+     if (input[4] !== undefined) {
         if (input[0] === input[4] && input[4] === input[8]) {
-            console.log('The winner is "' + input[0].toUpperCase() + '"');
+            showWinner('#cell-1 > h3' , '#cell-5 > h3' , '#cell-9 > h3' , letter);
             winner = true;
 
         }
-        if (input[2] === input[4] && input[4] === input[6]) {
-            console.log('The winner is "' + input[2].toUpperCase() + '"');
+        else if (input[2] === input[4] && input[4] === input[6]) {
+            showWinner('#cell-3 > h3' , '#cell-5 > h3' , '#cell-7 > h3' , letter);
             winner = true;
 
         }
-        if (input[1] === input[4] && input[4] === input[7]) {
-            console.log('The winner is "' + input[1].toUpperCase() + '"');
+        else if (input[1] === input[4] && input[4] === input[7]) {
+            showWinner('#cell-2 > h3' , '#cell-5 > h3' , '#cell-8 > h3' , letter);
             winner = true;
         }
-        if (input[3] === input[4] && input[4] === input[5]) {
-            console.log('The winner is "' + input[3].toUpperCase() + '"');
+        else if (input[3] === input[4] && input[4] === input[5]) {
+            showWinner('#cell-4 > h3' , '#cell-5 > h3' , '#cell-6 > h3' , letter);
             winner = true;
         }
     }
     if (input[6] !== undefined) {
 
         if (input[6] === input[7] && input[7] === input[8]) {
-            console.log('The winner is "' + input[6].toUpperCase() + '"');
+            showWinner('#cell-7 > h3' , '#cell-8 > h3' , '#cell-9 > h3' , letter);
             winner = true;
         }
+    }
+     if (tieCounter === 9 && winner !== true) {
+        console.log('There is a tie here');
     }
 };
 
@@ -84,6 +105,7 @@ const move = function (e) {
                 parseInt($(e.target).children('h3').attr('about'));
                 input[num] = 'x';
                 letter = false;
+                tiecounter++;
                 checkWinner();
             } else {
                 alert('Invalid space');
@@ -95,17 +117,28 @@ const move = function (e) {
                 let num = $(e.target).children('h3').attr('about');
                 parseInt($(e.target).children('h3').attr('about'));
                 input[num] = 'o';
+                tiecounter++;
                 checkWinner();
             }
         }
     }
     else {
-        alert('Game over');
+        resetBoard();
     }
 };
 
-const resetGame = function () {
-    location.reload();
+const resetBoard = function () {
+    $('.game-board div h3').text('');
+    $('.game-board div h3').removeClass('winner');
+    letter = false;
+    winner = false;
+    tieCounter = 0;
+    input.length = 0;
+
 };
-$('#reset').on('click', resetGame);
 $('.game-board').on('click', move);
+$('#reset').on('click' , resetBoard);
+$('img').on('click' , function() {
+    console.log('Nader is here again');
+});
+
